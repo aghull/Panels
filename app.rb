92b0d8c300beba125005f236a744128af11524d0
Andrew Hull <aghull@gmail.com>
2012-03-09 01:59:31 -0500

    name tag

--- app.rb
+++ app.rb
@@ -60,7 +60,8 @@ get %r{/(\d*)$} do
   @friends = @user.friends.sort_by{rand}.slice(0..15)
   @colors = [];
   @friend = params[:captures].first;
-  @friend = @friends[0].id if @friend.empty?;
+  @friend = @friends[rand @friends.count].id if @friend.empty?;
+  @name = Mogli::User.find(@friend, @client).name
 
   res = HTTParty::get 'https://graph.facebook.com/'+@friend+'/picture'
   img = (Image.from_blob res.body)[0]
